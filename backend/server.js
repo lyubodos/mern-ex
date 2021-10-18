57b86b210a26e429bfbd4ea2d6d48f772146c601
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-10-18 23:07:57 +0300

    Connect MongoDB with the express server js file
    Connected the MongoDb/test database colleaction with the main server app
    file of express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv/config");
 
 const userRouter = require("./routes/users");
 const exRouter = require("./routes/exercises");
@@ -10,6 +13,17 @@ app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 4000;
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri,  { useNewUrlParser: true});
+
+const connection = mongoose.connection;
+
+
+connection.once("open", () => {
+    console.log("MongoDB connection was established successfully")
+});
 
 
 app.use("/users", userRouter);
