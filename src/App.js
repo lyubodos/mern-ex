7a384de11a8c0c9e3b68d28ed486bbe03d15d874
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-10-20 22:01:51 +0300

    Add routing for the Create User Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import CreateUser from "./components/CreateUser";
 import ExerciseList from './components/ExerciseList';
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={ExerciseList}/>
+        <Header/>
 
+        <Route path="/" exact component={ExerciseList}/>
+        <Route path="/user" component={CreateUser}/>
       </Router>
     </div>
   );
