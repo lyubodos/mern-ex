3d1cba51287436cff09252951fc6b9e6b629b3f3
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-10-20 21:40:02 +0300

    Add basic input in the ExercisesList Component
    Basic input added such as state control, basic html struture for
    displaying the table's contents, which will be later called out.

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 
 export default class ExerciseList extends Component {
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            exercises: []
+        }
+    };
+
+
+    exercisesList(){
+        return this.state = exercises.join("");
+    }
+
     render() {
         return (
             <div>
-                
+                <h2>Logged Exercises:</h2>
+                <table className="table">
+                    <thead className="table-light"> 
+                        <tr>Username</tr>
+                        <tr>Description</tr>
+                        <tr>Duration</tr>
+                        <tr>Date</tr>
+                        <tr>Actions</tr>
+                    </thead>
+                    <tbody>
+                        {this.exercisesList()}
+                    </tbody>
+                </table>
             </div>
-        )
+        );
     }
 }
